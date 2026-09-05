83f99fb9d8e0404c5ab9b790a537e893b64688bd
Aayush Verma <aayushvermaav2005@gmail.com>
2026-09-05 14:40:27 +0530

    Initial AmbulanceOS project setup

---
+++ src/main/java/com/ambulanceos/AmbulanceOsApplication.java
@@ -0,0 +1,13 @@
+package com.ambulanceos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbulanceOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbulanceOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambulanceos/AmbulanceOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambulanceos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbulanceOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
